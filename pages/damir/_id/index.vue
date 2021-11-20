f425366f4994bcf6e9938346add2978c572cc0d5
Damir Akzhigitov <damir.ckelet@gmail.com>
2021-11-20 18:22:57 +0300

    some-proccess

---
+++ pages/damir/_id/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    {{ this.id }}
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
--- pages/damir/index.vue
+++ pages/damir/index.vue
@@ -1,69 +1,92 @@
 <template>
   <div>
-    <div v-for="room in rooms" :key="room.id">
-      <div>{{ room.name }}</div>
-      <div>
-        <button @click="enterRoom(room)">Войти</button>
+    <div v-if="rooms && rooms.length">
+      <div v-for="room in rooms" :key="room.id">
+        <div>{{ room.id }}</div>
+        <div>
+          <button @click="enterRoom(room)">Войти</button>
+        </div>
       </div>
     </div>
 
-    <button @click="fetchSomething">fetch</button>
+    <button @click="onClickCreateRoom">Создать комнату</button>
+    <button @click="onClickGetRooms">Поиск комнат</button>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+let headers = new Headers()
+
+headers.append('Content-Type', 'application/json')
+headers.append('Accept', 'application/json')
+headers.append('Origin', 'http://localhost:3000')
 
 const socket = ref(null)
 
-const rooms = ref([
-  {
-    name: 'Комната 1',
-    id: 1,
-  },
-  {
-    name: 'Комната 1',
-    id: 2,
-  },
-])
+const router = useRouter()
+
+const rooms = ref(null)
 
 onMounted(() => {
   socket.value = new WebSocket('ws://localhost:8080')
 
   socket.value.addEventListener('open', function (event) {
     socket.value.send('Hello Server!')
-    socket.value.send('Hello Server2!')
   })
 
   socket.value.addEventListener('message', function (event) {
     console.log('Message from server ', event.data)
+
+    document.cookie = `user=${event.data}`
   })
+
+  onClickGetRooms()
 })
 
-const enterRoom = (room) => {
+const enterRoom = async (room) => {
   console.log(`Вход в комнату: #${room.id} `)
+
+  try {
+    const response = await fetch('http://localhost:8080/enter', {
+      method: 'POST',
+      credentials: 'include',
+      body: JSON.stringify({ id: room.id }),
+    })
+
+    rooms.value = await response.json()
+
+    await router.push(`/damir/${room.id}`)
+  } catch (e) {
+    console.log(e)
+  }
 }
 
-const fetchSomething = async () => {
-  socket.value.send('Hello Server!')
+const onClickCreateRoom = async () => {
+  try {
+    const response = await fetch('http://localhost:8080/newRoom', {
+      credentials: 'same-origin',
+    })
 
-  console.log('socket.value: ', socket.value)
-  // await fetch('http://localhost:8080/damir', {
-  //   headers: {
-  //     Accept:
-  //       'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-  //     'Accept-Language': 'en-US,ru-RU;q=0.8,ru;q=0.5,en;q=0.3',
-  //     'Upgrade-Insecure-Requests': '1',
-  //     'Sec-Fetch-Dest': 'document',
-  //     'Sec-Fetch-Mode': 'navigate',
-  //     'Sec-Fetch-Site': 'none',
-  //     'Sec-Fetch-User': '?1',
-  //     'Cache-Control': 'max-age=0',
-  //   },
-  //   method: 'GET',
-  //   mode: 'no-cors',
-  // })
+    const data = await response.json()
+
+    rooms.value.push(data)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+const onClickGetRooms = async () => {
+  try {
+    const response = await fetch('http://localhost:8080/rooms', {
+      credentials: 'include',
+    })
+
+    rooms.value = await response.json()
+  } catch (e) {
+    console.log(e)
+  }
 }
 </script>
-
-<style scoped></style>
