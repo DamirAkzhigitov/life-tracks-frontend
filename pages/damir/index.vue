e6fc2a3f0a0991b1959db48061ba569f4ef8825a
Damir Akzhigitov <damir.ckelet@gmail.com>
2021-11-21 17:39:29 +0300

    refactoring

--- pages/damir/index.vue
+++ pages/damir/index.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
     <div v-if="rooms && rooms.length">
-      <div v-for="room in rooms" :key="room.id">
+      <div v-for="room in rooms" :key="room.id" class="room">
         <div>{{ room.id }}</div>
         <div>members: {{ room.members }}</div>
+        <div>свободно мест: {{ room.max - room.members.length }}</div>
         <div>
           <button @click="enterRoom(room)">Войти</button>
         </div>
       </div>
     </div>
-
-    <button @click="onClickCreateRoom">Создать комнату</button>
-    <button @click="onClickGetRooms">Поиск комнат</button>
+    <div class="button-container">
+      <button @click="onClickCreateRoom">Создать комнату</button>
+    </div>
   </div>
 </template>
 
@@ -23,7 +24,7 @@ let headers = new Headers()
 
 headers.append('Content-Type', 'application/json')
 headers.append('Accept', 'application/json')
-headers.append('Origin', 'http://localhost:3000')
+headers.append('Origin', 'http://192.168.1.68:3000')
 
 const socket = ref(null)
 
@@ -33,7 +34,7 @@ const rooms = ref([])
 
 const auth = async () => {
   try {
-    const response = await fetch('http://localhost:8080/auth', {
+    const response = await fetch('http://192.168.1.68:8080/auth', {
       method: 'GET',
       credentials: 'include',
     })
@@ -44,25 +45,16 @@ const auth = async () => {
   } catch (e) {
     console.log(e)
   }
-
-  // console.log('enterRoom after rooms.value: ', rooms.value)
 }
 
 const enterRoom = async (room) => {
-  // console.log(`Вход в комнату: #${room.id} `)
-  //
-  // console.log('enterRoom before rooms.value: ', rooms.value)
-
   try {
-    const response = await fetch('http://localhost:8080/enter', {
+    await fetch('http://192.168.1.68:8080/enter', {
       method: 'POST',
       credentials: 'include',
+      mode: 'cors',
       body: JSON.stringify({ id: room.id }),
     })
-
-    const data = await response.json()
-
-    // console.log(data.text)
   } catch (e) {
     console.log(e)
   }
@@ -72,13 +64,9 @@ const enterRoom = async (room) => {
 
 const onClickCreateRoom = async () => {
   try {
-    const response = await fetch('http://localhost:8080/newRoom', {
-      credentials: 'same-origin',
+    await fetch('http://192.168.1.68:8080/newRoom', {
+      credentials: 'include',
     })
-
-    const data = await response.json()
-
-    // rooms.value.push(data)
   } catch (e) {
     console.log(e)
   }
@@ -86,7 +74,7 @@ const onClickCreateRoom = async () => {
 
 const onClickGetRooms = async () => {
   try {
-    const response = await fetch('http://localhost:8080/rooms', {
+    const response = await fetch('http://192.168.1.68:8080/rooms', {
       credentials: 'include',
     })
 
@@ -99,7 +87,7 @@ const onClickGetRooms = async () => {
 onMounted(async () => {
   await auth()
 
-  socket.value = new WebSocket('ws://localhost:8080')
+  socket.value = new WebSocket('ws://192.168.1.68:8080')
 
   socket.value.addEventListener('open', function (event) {
     // socket.value.send('Hello Server!')
@@ -108,11 +96,7 @@ onMounted(async () => {
   socket.value.addEventListener('message', function (event) {
     console.log('new message from WS')
     try {
-      const roomData = JSON.parse(event.data)
-
-      console.log('message: ', roomData)
-
-      rooms.value = roomData
+      rooms.value = JSON.parse(event.data)
     } catch (e) {
       console.log('error = ', e)
     }
@@ -123,3 +107,15 @@ onMounted(async () => {
   await onClickGetRooms()
 })
 </script>
+
+<style scoped>
+.room {
+  border: 1px solid black;
+  padding: 20px;
+  margin: 10px;
+}
+.button-container {
+  padding: 20px;
+  margin: 10px;
+}
+</style>
